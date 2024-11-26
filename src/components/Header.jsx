9a456ccd1973dbc713ef23381e0213131920405a
Tadas Darguzis <darguzistadas@gmail.com>
2024-11-26 11:30:53 +0100

    Uploading repository to github

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to my website!</h1>
+      <nav>
+        <a>Home</a>
+        <a>About</a>
+        <a>Contact</a>
+      </nav>
+    </header>
+  );
+};
+export default Header;
